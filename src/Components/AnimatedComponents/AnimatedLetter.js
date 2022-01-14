405f51aab8205cf14ea05858a0ba167fc75f0fb2
Rick Brown <ricky.brown.00@gmail.com>
2022-01-14 16:30:41 +0000

    adds animiated word

---
+++ src/Components/AnimatedComponents/AnimatedLetter.js
@@ -0,0 +1,35 @@
+import React, { useCallback, useEffect, useState } from 'react'
+
+const AnimatedLetter = ({ children, index }) => {
+	const [hovered, setHovered] = useState(false)
+
+	const animationTiming = useCallback(() => {
+		setHovered(true)
+		setTimeout(() => {
+			setHovered(false)
+		}, 2000 + index * 100)
+	}, [index])
+
+	useEffect(() => {
+		setTimeout(() => {
+			animationTiming()
+		}, 1000 + index * 100)
+
+		return () => setHovered(false)
+	}, [animationTiming, index])
+
+	setInterval(() => {
+		animationTiming()
+	}, 10000 + index * 100)
+
+	return (
+		<span
+			className={hovered ? 'rubberBand animated' : 'rubberBand'}
+			onMouseOver={animationTiming}
+		>
+			{children}
+		</span>
+	)
+}
+
+export default AnimatedLetter
