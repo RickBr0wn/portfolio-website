84ca6fd07cc4ff440821e23298919611c4846b06
Rick Brown <ricky.brown.00@gmail.com>
2022-02-01 14:35:56 +0000

    adds hooklines and image

--- src/Components/LayoutContainer.js
+++ src/Components/LayoutContainer.js
@@ -29,25 +29,27 @@ const NavigationBar = styled.nav`
 	justify-content: space-between;
 `
 
-const NavigationItems = styled.a`
+const NavigationItems = styled.h4`
 	display: flex;
-	justify-content: space-between;
-	width: 40vw;
-	padding: 2rem;
+	justify-content: space-evenly;
+	width: 30vw;
+	padding: 3rem 0rem;
+	text-transform: uppercase;
 `
-
 const NavigationLogo = styled.div`
 	padding: 2rem;
 `
 
-const NavigationLink = styled.div``
+const NavigationLink = styled.div`
+	cursor: pointer;
+`
 
 const LayoutContainer = ({ children }) => (
 	<Layout>
 		<NavigationBar>
-			<NavigationLogo>RB</NavigationLogo>
+			<NavigationLogo>{''}</NavigationLogo>
 			<NavigationItems>
-				<NavigationLink>about</NavigationLink>
+				{/* <NavigationLink>about</NavigationLink> */}
 				<NavigationLink>blog</NavigationLink>
 				<NavigationLink>projects</NavigationLink>
 				<NavigationLink>contact me</NavigationLink>
@@ -55,9 +57,9 @@ const LayoutContainer = ({ children }) => (
 		</NavigationBar>
 		{children}
 		<footer>
-			<Link href='mailto:info@rick.brown.co.uk'>
+			{/* <Link href='mailto:info@rick.brown.co.uk'>
 				<span className='link'>info@rick.brown.co.uk</span>
-			</Link>
+			</Link> */}
 		</footer>
 	</Layout>
 )
--- src/Pages/App.js
+++ src/Pages/App.js
@@ -3,13 +3,36 @@ import { Theme } from '../Theme/ColorTheme'
 import LayoutContainer from '../Components/LayoutContainer'
 // import SiteUnderConstruction from '../Components/SiteUnderConstruction'
 import AnimatedWord from '../Components/AnimatedWord'
+import styled from 'styled-components'
+import image from '../assets/image.png'
+
+const TextContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	margin-top: -16rem;
+	position: relative;
+`
+
+const Image = styled.img`
+	height: ${props => props.size}rem;
+	width: ${props => props.size}rem;
+	position: absolute;
+	top: -0.4rem;
+	left: -20rem;
+`
 
 function App() {
 	return (
 		<ThemeProvider theme={Theme}>
 			<LayoutContainer>
 				{/* <SiteUnderConstruction /> */}
-				<AnimatedWord word={Array.from('Rick-Brown')} />
+				<TextContainer>
+					<Image size={18} src={image} />
+					<h2> Hello, I'm</h2>
+					<AnimatedWord word={Array.from('Rick-Brown')} />
+					<h2>and I create unique web and iOS experiences.</h2>
+				</TextContainer>
 			</LayoutContainer>
 		</ThemeProvider>
 	)
